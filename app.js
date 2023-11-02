226170af2095a74ffe58e8dda7dcb89cf76d1e51
Katarzyna Fronc <katarzynaanna.fronc@gmail.com>
2023-11-02 11:52:44 +0100

    displaying piano roll cards without clicking the button

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import PianoRoll from './pianoroll.js';
+import PianoRoll from "./pianoroll.js";
 
 class PianoRollDisplay {
   constructor(csvURL) {
@@ -8,49 +8,49 @@ class PianoRollDisplay {
 
   async loadPianoRollData() {
     try {
-      const response = await fetch('https://pianoroll.ai/random_notes');
+      const response = await fetch("https://pianoroll.ai/random_notes");
       if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
       }
       this.data = await response.json();
     } catch (error) {
-      console.error('Error loading data:', error);
+      console.error("Error loading data:", error);
     }
   }
 
   preparePianoRollCard(rollId) {
-    const cardDiv = document.createElement('div');
-    cardDiv.classList.add('piano-roll-card');
+    const cardDiv = document.createElement("div");
+    cardDiv.classList.add("piano-roll-card");
 
     // Create and append other elements to the card container as needed
-    const descriptionDiv = document.createElement('div');
-    descriptionDiv.classList.add('description');
+    const descriptionDiv = document.createElement("div");
+    descriptionDiv.classList.add("description");
     descriptionDiv.textContent = `This is a piano roll number ${rollId}`;
     cardDiv.appendChild(descriptionDiv);
 
-    const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-    svg.classList.add('piano-roll-svg');
-    svg.setAttribute('width', '80%');
-    svg.setAttribute('height', '150');
+    const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    svg.classList.add("piano-roll-svg");
+    svg.setAttribute("width", "80%");
+    svg.setAttribute("height", "150");
 
     // Append the SVG to the card container
     cardDiv.appendChild(svg);
 
-    return { cardDiv, svg }
+    return { cardDiv, svg };
   }
 
   async generateSVGs() {
     if (!this.data) await this.loadPianoRollData();
     if (!this.data) return;
-    
-    const pianoRollContainer = document.getElementById('pianoRollContainer');
-    pianoRollContainer.innerHTML = '';
+
+    const pianoRollContainer = document.getElementById("pianoRollContainer");
+    pianoRollContainer.innerHTML = "";
     for (let it = 0; it < 20; it++) {
       const start = it * 60;
       const end = start + 60;
       const partData = this.data.slice(start, end);
 
-      const { cardDiv, svg } = this.preparePianoRollCard(it)
+      const { cardDiv, svg } = this.preparePianoRollCard(it);
 
       pianoRollContainer.appendChild(cardDiv);
       const roll = new PianoRoll(svg, partData);
@@ -58,7 +58,10 @@ class PianoRollDisplay {
   }
 }
 
-document.getElementById('loadCSV').addEventListener('click', async () => {
-  const csvToSVG = new PianoRollDisplay();
-  await csvToSVG.generateSVGs();
-});
+const csvToSVG = new PianoRollDisplay();
+csvToSVG
+  .generateSVGs()
+  .then(() => {})
+  .catch((error) => {
+    console.error(error);
+  });
